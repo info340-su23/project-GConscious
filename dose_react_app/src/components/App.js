a8cb2825853a7e37471964edcad822e477b9f4fe
labram13 <labram@uw.edu>
2023-08-01 00:20:58 -0700

    created starter components and applied Navbar and footer to render. Added css style from project to current react index.css

--- dose_react_app/src/components/App.js
+++ dose_react_app/src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Header } from './Header.js';
+import { Footer } from './Footer.js';
 
 
 function App() {
   return (
     <div>
-      test
+
+      <Header />
+      {/* <Main />  */}
+      <Footer />
+
     </div>
   );
 }
---
+++ dose_react_app/src/components/NavBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export function NavBar() {
+    return (
+        <nav class="navbar navbar-expand-lg py-0">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Dose</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a aria-label="My Pillbox Page" class="nav-link" href="index.html">My Pillbox</a>
+                        </li>
+                        <li class="nav-item">
+                            <a aria-label="Search Page" class="nav-link" href="Static_MockUp/Search.html">Search</a>
+                        </li>
+                        <li class="nav-item">
+                            <a aria-label="Upload Meds Page" class="nav-link" href="Static_MockUp/Upload.html">Upload Meds</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
