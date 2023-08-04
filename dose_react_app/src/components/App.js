2a205109eb79dfc0c2ea5fc2d0c25c2588d33869
labram13 <labram@uw.edu>
2023-08-03 19:31:47 -0700

    pillbox content changes

--- dose_react_app/src/components/App.js
+++ dose_react_app/src/components/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
+
 import { Header } from './Header.js';
 import { Footer } from './Footer.js';
 import { PillboxContent } from './SectionPillboxContent.js'
+
+const SAMPLE_DATA =
+{
+  pillName: "advil",
+  days: ["monday", "tuesday", "wednesday"],
+  dose: 3,
+  quantity: 40,
+  refills: 3,
+  description: "For headaches, fevers, inflammation, hangovers",
+  symptoms: "feel better i guess?"
+}
+console.log(SAMPLE_DATA);
 
 function App() {
   return (
     <div>
 
       <Header />
-      <PillboxContent 
-        img="../img/days/monday.png"
-        altText = "Image of dates in a week"
-      />
-      {/* <Main />  */}
-      <Footer />
-
+      <main>
+        <div className="container">
+          <PillboxContent sampleData={SAMPLE_DATA} />
+          <Footer />
+        </div>
+      </main>
     </div>
   );
 }
