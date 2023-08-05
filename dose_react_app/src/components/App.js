8e358f6f46c4d35caecb890a0092b43282e0bc56
labram13 <labram@uw.edu>
2023-08-05 15:43:58 -0700

    Co-authored-by: ericf05 <ericf05@users.noreply.github.com>
    Pillboxcontent items update

--- dose_react_app/src/components/App.js
+++ dose_react_app/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Header } from './Header.js';
 import { Footer } from './Footer.js';
-import { PillboxContent } from './SectionPillboxContent.js';
+import { PillboxRow } from './SectionPillboxContent.js';
 import { UploadForm } from './UploadForm.js';
 import { Route, Routes, Navigate, Outlet } from "react-router-dom";
 
@@ -22,7 +22,7 @@ const SAMPLE_DATA =
   [
     {
       pillName: "Advil",
-      days: ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
+      days: ["monday", "tuesday", "wednesday"],
       dose: 3,
       quantity: 40,
       refills: 3,
@@ -32,7 +32,16 @@ const SAMPLE_DATA =
 
     {
       pillName: "Chillpill",
-      days: ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
+      days: ["monday", "tuesday", "wednesday"],
+      dose: 3,
+      quantity: 40,
+      refills: 3,
+      description: "For headaches, fevers, inflammation, hangovers",
+      symptoms: "feel better i guess?"
+    },
+    {
+      pillName: "hangover pill",
+      days: ["friday"],
       dose: 3,
       quantity: 40,
       refills: 3,
@@ -40,7 +49,25 @@ const SAMPLE_DATA =
       symptoms: "feel better i guess?"
     }
   ]
-console.log(SAMPLE_DATA);
+
+// USE STATE PSEUDO CODE
+// const [userData, setUserdata] = useState(SAMPLE_DATA);
+
+// const handleClick = (event) (
+
+//   setUserData(formUploadObj) 
+// )
+
+
+
+const weeklyPills = { monday: [], tuesday: [], wednesday: [], thursday: [], friday: [], saturday: [], sunday: [] };
+
+SAMPLE_DATA.forEach(pillObj => {
+  pillObj.days.forEach(day => {
+    weeklyPills[day].push(pillObj)
+  })
+})
+console.log(weeklyPills);
 
 function App() {
   return (
@@ -49,8 +76,7 @@ function App() {
       <main>
         <div className="container">
           <Routes>
-
-            <Route path="pillbox" element={<PillboxContent sampleData={SAMPLE_DATA} />} />
+            <Route path="pillbox" element={<PillboxRow weeklyPills={weeklyPills} />} />
             <Route path="upload" element={<UploadForm />} />
 
           </Routes>
--- dose_react_app/src/components/SectionPillboxContent.js
+++ dose_react_app/src/components/SectionPillboxContent.js
@@ -1,55 +1,108 @@
 import React, { useState } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 
-function PillboxCardContents(props) {
-  const { pillName, dose, quantity, refills, description, symptoms } = props.dayData;
-  /*const sampleData = props.sampleData;
-  console.log(sampleData.pillName);*/
-
+function PillItems(props) {
+  const day = props.day;
+  const weeklyPills = props.weeklyPills;
   return (
-    <div className="card my-2" style={{ width: '18rem' }}>
-      <img src={require('../days/' + props.dayName + '.png')} alt="React Image" />
-      <Accordion defaultActiveKey="1">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>{pillName}</Accordion.Header>
-          <Accordion.Body>
-            <p>{"Dose: " + dose + " per day."}</p>
-            <p>{"Quantity: " + quantity}</p>
-            <p>{"Next Refill: " + refills + " weeks left. "}</p>
-            <p>{"Description: " + description}</p>
-            <p>{"Symptoms: " + symptoms}</p>
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
-    </div>
-  );
+
+
+
+    weeklyPills[day].map(pillObj => {
+      if (weeklyPills[day].length === 0) {
+        return <p>No pills for this day</p>
+      } else {
+        return (
+          <Accordion.Item key={pillObj.pillName} eventKey="0">
+            <Accordion.Header>{pillObj.pillName}</Accordion.Header>
+            <Accordion.Body>
+              <p>{"Dose: " + pillObj.dose + " per day."}</p>
+              <p>{"Quantity: " + pillObj.quantity}</p>
+              <p>{"Next Refill: " + pillObj.refills + " weeks left. "}</p>
+              <p>{"Description: " + pillObj.description}</p>
+              <p>{"Symptoms: " + pillObj.symptoms}</p>
+            </Accordion.Body>
+          </Accordion.Item>
+        )
+      }
+    })
+  )
 }
 
 
+function PillboxContent(props) {
+  const weeklyPills = props.weeklyPills;
+  const daysOfTheWeek = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+  return (
+    daysOfTheWeek.map(day => {
 
-export function PillboxContent(props) {
-  const sampleData = props.sampleData;
-  // const { days } = props.sampleData;
-  // days.map((dayName, index) => (
-  //   <div key={index} className="col-12 col-md-6 col-lg-4 d-flex align-items-start">
-  //     <PillboxCardContents dayData={props.sampleData} dayName={dayName} />
-  //   </div>
-  // ))
+      return (
+
+        <div key={day} className="col-12 col-md-6 col-lg-4 d-flex align-items-start">
+          <div className="card my-2" style={{ width: '18rem' }}>
+            <img src={require('../days/' + day + '.png')} alt="React Image" />
+            <Accordion defaultActiveKey="1">
+              <PillItems day={day} weeklyPills={weeklyPills} />
+            </Accordion>
+          </div>
+        </div>
+      )
+    })
+  )
+
+
+
+}
+
+export function PillboxRow(props) {
+  const weeklyPills = props.weeklyPills
   return (
     <div className="row my-4">
-      {
-        sampleData.map((pillObj) => {
-          console.log(pillObj);
-          const { days } = pillObj;
-          return (
-            days.map((dayName, index) => (
-              <div key={index} className="col-12 col-md-6 col-lg-4 d-flex align-items-start">
-                <PillboxCardContents dayData={pillObj} dayName={dayName} />
-              </div>
-            ))
-          )
-        })
-      }
+      <PillboxContent weeklyPills={weeklyPills} />
     </div>
-  );
+  )
+
 }
+
+
+
+
+
+    // daysOfTheWeek.map(day => {
+    //   <div class="row my-4">
+    //     <div className="col-12 col-md-6 col-lg-4 d-flex align-items-start">
+    //       <div className="card my-2" style={{ width: '18rem' }}>
+    //         <img src={require('../days/' + day + '.png')} alt="React Image" />
+    //         <Accordion defaultActiveKey="0">
+    //           <Accordion.Item eventKey="0">
+    //             <Accordion.Header>Accordion Item #1</Accordion.Header>
+    //             <Accordion.Body>
+    //               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    //               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+    //               minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+    //               aliquip ex ea commodo consequat. Duis aute irure dolor in
+    //               reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    //               pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    //               culpa qui officia deserunt mollit anim id est laborum.
+    //             </Accordion.Body>
+    //           </Accordion.Item>
+    //           <Accordion.Item eventKey="1">
+    //             <Accordion.Header>Accordion Item #2</Accordion.Header>
+    //             <Accordion.Body>
+    //               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    //               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+    //               minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+    //               aliquip ex ea commodo consequat. Duis aute irure dolor in
+    //               reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    //               pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    //               culpa qui officia deserunt mollit anim id est laborum.
+    //             </Accordion.Body>
+    //           </Accordion.Item>
+    //         </Accordion>
+/* <Accordion defaultActiveKey="1">
+    </Accordion> */
+    //     </div>
+    //   </div>
+    // </div>
+    // })
+
