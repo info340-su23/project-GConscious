522615453edd372bd2bd788aea1ec32eb2093405
samayabadrieh <samayabadrieh@gmail.com>
2023-08-16 18:27:40 -0500

    add pseudocode for reorganize reactivity

---
+++ src/components/ReorganizePillbox.js
@@ -0,0 +1,17 @@
+// NOTE: COMMENT OUT THE FOLLOWING CODE TO BEGIN IMPLEMENTING PSEUDOCODE
+
+// import React from 'react';
+
+// function Reorganize(props) {
+//     // Grab passd down current Prescriptions and list them out for user
+
+//     // Have user select which pill to adjust days
+
+//     // Grab pillobj[days] in the following format: const newPrescription = {pillName: pillName, dose: dose, quantity: quantity}
+
+//     // Re-assign/replace days with user's chosen days
+
+//     // setUserPrescription with modified pill
+
+//     return()
+// }
